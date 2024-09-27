cd69663987e91208a2344fb72a4134f2582fecd3
Callum Campbell <2719717c@student.gla.ac.uk>
2024-09-27 11:07:52 +0100

    Add placeholder text props

--- src/constants/ClubConstants.ts
+++ src/constants/ClubConstants.ts
@@ -5,7 +5,7 @@ export const CLUBS = {
     [COUNTRIES.GERMANY]: ['bayern-munich', 'borussia-dortmund', 'hertha-bsc', 'hamburger-sv', 'monchengladbach'],
     [COUNTRIES.SPAIN]: ['barcelona', 'atletico-madrid', 'valencia', 'sevilla', 'real-betis'],
     [COUNTRIES.ITALY]: ['inter-milan', 'ac-milan', 'as-roma', 'napoli', 'juventus'],
-    [COUNTRIES.SCOTLAND]: ['rangers', 'celtic'],
+    [COUNTRIES.SCOTLAND]: [ 'celtic', 'rangers'],
     [COUNTRIES.DENMARK]: ['fc-copenhagen', 'brondby'],
     [COUNTRIES.FRANCE]: ['psg', 'marseille', 'lyon', 'monaco', 'nice'],
     [COUNTRIES.PORTUGAL]: ['benfica', 'sporting-lisbon', 'porto'],
