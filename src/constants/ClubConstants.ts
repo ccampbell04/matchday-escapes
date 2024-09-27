89e4cc9b873c7a500a7d60f2519a12b3f4519c16
Callum Campbell <93252952+ccampbell04@users.noreply.github.com>
2024-09-27 10:21:51 +0100

    Update ClubConstants.ts

--- src/constants/ClubConstants.ts
+++ src/constants/ClubConstants.ts
@@ -4,8 +4,12 @@ export const CLUBS = {
     [COUNTRIES.NETHERLANDS]: ['ajax', 'psv', 'feyenoord'],
     [COUNTRIES.GERMANY]: ['borussia-dortmund', 'hertha-bsc', 'hamburger-sv', 'monchengladbach'],
     [COUNTRIES.SPAIN]: ['barcelona', 'atletico-madrid'],
-    [COUNTRIES.ITALY]: ['inter-milan', 'ac-milan', 'as-roma', 'napoli'],
+    [COUNTRIES.ITALY]: ['inter-milan', 'ac-milan', 'as-roma', 'napoli', 'juventus'],
     [COUNTRIES.SCOTLAND]: ['rangers', 'celtic'],
     [COUNTRIES.DENMARK]: ['fc-copenhagen', 'brondby'],
-    [COUNTRIES.FRANCE]: ['psg', 'marseille', 'lyon', 'monaco', 'nice']
+    [COUNTRIES.FRANCE]: ['psg', 'marseille', 'lyon', 'monaco', 'nice'],
+    [COUNTRIES.PORTGUAL]: ['benfica', 'sporting-lisbon', 'porto'],
+    [COUNTRIES.SWITZERLAND]: ['basel' 'zurich', 'young-boys'],
+    [COUNTRIES.CZECH]: ['sparta', 'slavia'],
+    [COUNTRIES.HUNGARY]: ['ferencvarosi']
 }
