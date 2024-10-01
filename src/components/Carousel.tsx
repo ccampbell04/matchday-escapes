c97a0497b75b987cfaa486dddc7f6c6ddffe0b8f
Callum Campbell <2719717c@student.gla.ac.uk>
2024-10-01 11:08:17 +0100

    CLS fix attempt #3

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -17,62 +17,76 @@ export function Carousel({
 }) {
   if (!isCarouselPage(clubName)) {
     return (
-      <div className="carousel-item-container">
+      <div>
         <center>
           <h2 className="display-6">Previous Trips</h2>
         </center>
-        
+
         <CCarousel controls indicators>
-            <CCarouselItem>
+          <CCarouselItem>
+            <div className="carousel-item-container">
               <CImage
                 className="d-block w-100"
                 src={`/images/ajax/carousel1.JPEG`}
                 alt="slide 1"
               />
-              <CCarouselCaption className="d-none d-md-block">
-                <h5>Ajax Amsterdam</h5>
-                <p>Ajax vs AEK Athens</p>
-              </CCarouselCaption>
-            </CCarouselItem>
+            </div>
+            <CCarouselCaption className="d-none d-md-block">
+              <h5>Ajax Amsterdam</h5>
+              <p>Ajax vs AEK Athens</p>
+            </CCarouselCaption>
+          </CCarouselItem>
+
           <CCarouselItem>
-            <CImage
-              className="d-block w-100"
-              src={`/images/hertha-bsc/carousel3.JPEG`}
-              alt="slide 2"
-            />
+            <div className="carousel-item-container">
+              <CImage
+                className="d-block w-100"
+                src={`/images/hertha-bsc/carousel3.JPEG`}
+                alt="slide 2"
+              />
+            </div>
             <CCarouselCaption className="d-none d-md-block">
               <h5>Hertha BSC</h5>
               <p>Hertha BSC vs Borussia Dortmund</p>
             </CCarouselCaption>
           </CCarouselItem>
+
           <CCarouselItem>
-            <CImage
-              className="d-block w-100"
-              src={`/images/inter-milan/carousel1.JPEG`}
-              alt="slide 3"
-            />
+            <div className="carousel-item-container">
+              <CImage
+                className="d-block w-100"
+                src={`/images/inter-milan/carousel1.JPEG`}
+                alt="slide 3"
+              />
+            </div>
             <CCarouselCaption className="d-none d-md-block">
               <h5>Internazionale Milano</h5>
               <p>Inter vs Bologna</p>
             </CCarouselCaption>
           </CCarouselItem>
+
           <CCarouselItem>
-            <CImage
-              className="d-block w-100"
-              src={`/images/ajax/carousel3.JPEG`}
-              alt="slide 3"
-            />
+            <div className="carousel-item-container">
+              <CImage
+                className="d-block w-100"
+                src={`/images/ajax/carousel3.JPEG`}
+                alt="slide 3"
+              />
+            </div>
             <CCarouselCaption className="d-none d-md-block">
               <h5>Ajax Amsterdam</h5>
               <p>Ajax VS Almere City FC</p>
             </CCarouselCaption>
           </CCarouselItem>
+
           <CCarouselItem>
-            <CImage
-              className="d-block w-100"
-              src={`/images/fc-copenhagen/carousel1.JPEG`}
-              alt="slide 3"
-            />
+            <div className="carousel-item-container">
+              <CImage
+                className="d-block w-100"
+                src={`/images/fc-copenhagen/carousel1.JPEG`}
+                alt="slide 3"
+              />
+            </div>
             <CCarouselCaption className="d-none d-md-block">
               <h5>FC Copenhagen</h5>
               <p>FC Copenhagen vs Kilmarnock FC</p>
