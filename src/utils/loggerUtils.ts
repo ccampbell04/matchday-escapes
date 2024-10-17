668dd144cd54a3458d5ac8b2fdbb6bbb60e8873a
callumcampbell <2719717c@student.gla.ac.uk>
2024-10-17 17:20:44 +0100

    No colour

--- src/utils/loggerUtils.ts
+++ src/utils/loggerUtils.ts
@@ -9,7 +9,6 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.Console({
       format: winston.format.combine(
-        winston.format.colorize(),
         winston.format.simple() 
       ),
     }),
